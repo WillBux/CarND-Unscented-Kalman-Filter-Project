3538074bf3e33d214e3e1c5e111f1fd914e16c0f
Will Buxton <will.buxton88@gmail.com>
2017-06-16 16:53:23 -0700

    Add files via upload

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,28 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+    //Calculate the error using the Root-Mean-Squared Error
+    VectorXd rmse(4);
+    rmse << 0,0,0,0;
+    //making sure that the RMSE calculations do not break
+    if(estimations.size() == 0){
+        cout << "estimations.size() == 0" << endl;
+        return rmse;
+    }
+    if(estimations.size() != ground_truth.size()){
+        cout << "estimations.size() != ground_truth.size()" << endl;
+        return rmse;
+    }
+    // Get the squared residuals
+    for(unsigned int i=0; i < estimations.size(); ++i){
+        VectorXd residual = estimations[i] - ground_truth[i];
+        residual = residual.array()*residual.array();
+        rmse += residual;
+    }
+
+    //Get the mean
+    rmse = rmse / estimations.size();
+    rmse = rmse.array().sqrt();
+    return rmse;
+
 }
